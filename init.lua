1ac9fc7effef0542ab200532b5146a9328c26100
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-08-06 19:41:59 +0100

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- Plugin management via Packer
+require("plugins")
+
+-- All non plugin related (vim) options
+require("options")
+
+-- Config Plugine
+require("config")
+
+-- Vim mappings, see lua/config/which.lua for more mappings
+require("mappings")
